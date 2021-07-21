deea0f42b7fbe8fa44390505dccd64b1346dcdbd
d_winter <2624376436@qq.com>
2021-07-21 14:13:57 +0800

    Update getUrlParams.ts

--- src/config/getUrlParams.ts
+++ src/config/getUrlParams.ts
@@ -1,6 +1,17 @@
 import { chainInfo } from './chainConfig'
 import { isAddress } from '../utils'
 
+import {ARBITRUM_MAIN_CHAINID} from './chainConfig/arbitrum'
+import {AVAX_MAIN_CHAINID} from './chainConfig/avax'
+import {BNB_MAIN_CHAINID, BNB_TEST_CHAINID} from './chainConfig/bsc'
+import {ETH_MAIN_CHAINID, ETH_TEST_CHAINID} from './chainConfig/eth'
+import {FSN_MAIN_CHAINID, FSN_TEST_CHAINID} from './chainConfig/fsn'
+import {FTM_MAIN_CHAINID} from './chainConfig/ftm'
+import {HT_MAIN_CHAINID, HT_TEST_CHAINID} from './chainConfig/ht'
+import {MATIC_MAIN_CHAINID} from './chainConfig/matic'
+import {XDAI_MAIN_CHAINID} from './chainConfig/xdai'
+import {KCC_MAIN_CHAINID} from './chainConfig/kcc'
+
 export function getParams(param: any) {
   const str = window.location.href.indexOf('?') ? window.location.href.split('?')[1] : ''
   if (str) {
@@ -38,22 +49,35 @@ export function getInitBridgeChain(destChainID: any, bridgeToken: any) {
 
 
 function getParamNode(type: any, INIT_NODE: any) {
-  if (type === 'fusion' || type === '32659') {
-    return chainInfo['32659'].label
-  } else if (type === 'fusiontestnet' || type === '46688') {
-    return chainInfo['46688'].label
-  } else if (type === 'bsc' || type === '56') {
-    return chainInfo['56'].label
-  } else if (type === 'bsctestnet' || type === '97') {
-    return chainInfo['97'].label
-  } else if (type === 'fantom' || type === '250') {
-    return chainInfo['250'].label
-  } else if (type === 'eth' || type === '1') {
-    return chainInfo['1'].label
-  } else if (type === 'huobi' || type === '128') {
-    return chainInfo['128'].label
-  } else if (type === 'huobitestnet' || type === '256') {
-    return chainInfo['256'].label
+  type = type?.toString()?.toLowerCase()
+  if (type === 'fusion' || type === 'fsn' || type.toString() === FSN_MAIN_CHAINID) {
+    return chainInfo[FSN_MAIN_CHAINID].label
+  } else if (type === 'fusiontestnet' || type === 'fsntestnet' || type.toString() === FSN_TEST_CHAINID) {
+    return chainInfo[FSN_TEST_CHAINID].label
+  } else if (type === 'bsc' || type.toString() === BNB_MAIN_CHAINID) {
+    return chainInfo[BNB_MAIN_CHAINID].label
+  } else if (type === 'bsctestnet' || type.toString() === BNB_TEST_CHAINID) {
+    return chainInfo[BNB_TEST_CHAINID].label
+  } else if (type === 'fantom' || type === 'ftm' || type.toString() === FTM_MAIN_CHAINID) {
+    return chainInfo[FTM_MAIN_CHAINID].label
+  } else if (type === 'eth' || type.toString() === ETH_MAIN_CHAINID) {
+    return chainInfo[ETH_MAIN_CHAINID].label
+  } else if (type === 'ethtestnet' || type.toString() === ETH_TEST_CHAINID) {
+    return chainInfo[ETH_TEST_CHAINID].label
+  } else if (type === 'huobi' || type === 'ht' || type.toString() === HT_MAIN_CHAINID) {
+    return chainInfo[HT_MAIN_CHAINID].label
+  } else if (type === 'huobitestnet' || type === 'httestnet' || type.toString() === HT_TEST_CHAINID) {
+    return chainInfo[HT_TEST_CHAINID].label
+  } else if (type === 'polygon' || type === 'matic' || type.toString() === MATIC_MAIN_CHAINID) {
+    return chainInfo[MATIC_MAIN_CHAINID].label
+  } else if (type === 'xdai' || type.toString() === XDAI_MAIN_CHAINID) {
+    return chainInfo[XDAI_MAIN_CHAINID].label
+  } else if (type === 'avalanche' || type === 'avax' || type.toString() === AVAX_MAIN_CHAINID) {
+    return chainInfo[AVAX_MAIN_CHAINID].label
+  } else if (type === 'arbitrum' || type.toString() === ARBITRUM_MAIN_CHAINID) {
+    return chainInfo[ARBITRUM_MAIN_CHAINID].label
+  } else if (type === 'kcc' || type.toString() === KCC_MAIN_CHAINID) {
+    return chainInfo[KCC_MAIN_CHAINID].label
   } else {
     return INIT_NODE
   }
@@ -61,15 +85,15 @@ function getParamNode(type: any, INIT_NODE: any) {
 
 function getNode(type: any, INIT_NODE: any) {
   if (type.indexOf('fsn') !== -1) {
-    return chainInfo['32659'].label
+    return chainInfo[FSN_MAIN_CHAINID].label
   } else if (type.indexOf('bsc') !== -1) {
-    return chainInfo['56'].label
+    return chainInfo[BNB_MAIN_CHAINID].label
   } else if (type.indexOf('ftm') !== -1) {
-    return chainInfo['250'].label
+    return chainInfo[FTM_MAIN_CHAINID].label
   } else if (type.indexOf('eth') !== -1) {
-    return chainInfo['1'].label
+    return chainInfo[ETH_MAIN_CHAINID].label
   } else if (type.indexOf('huobi') !== -1) {
-    return chainInfo['128'].label
+    return chainInfo[HT_MAIN_CHAINID].label
   } else {
     return INIT_NODE
   }
