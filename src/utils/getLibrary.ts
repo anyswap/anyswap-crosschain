edb884325bcade52447d4544922f0ad2b03d5ff2
d_winter <2624376436@qq.com>
2021-09-07 14:30:11 +0800

    Update getLibrary.ts

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -3,7 +3,7 @@ import { Web3Provider } from '@ethersproject/providers'
 export default function getLibrary(provider: any): any {
   if (provider) {
     const library = new Web3Provider(provider, 'any')
-    library.pollingInterval = 2500
+    library.pollingInterval = 2000
     return library
   }
   return ''
