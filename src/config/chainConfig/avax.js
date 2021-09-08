6fa0faf78880d2cf20067c3d22bb206438822e1c
d_winter <2624376436@qq.com>
2021-09-08 19:02:54 +0800

    optimized page view

--- src/config/chainConfig/avax.js
+++ src/config/chainConfig/avax.js
@@ -22,7 +22,7 @@ const bridgeToken = {
     nativeToken: ''
   },
   [VERSION.V5]: {
-    bridgeInitToken: '',
+    bridgeInitToken: '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664',
     bridgeInitChain: '56',
     nativeToken: '',
     crossBridgeInitToken: '0xb44a9b6905af7c801311e8f4e76932ee959c663c'
