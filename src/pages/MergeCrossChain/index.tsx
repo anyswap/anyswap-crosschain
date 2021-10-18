aa9e50879171d16070593a3ec2ade10aee40da2a
d_winter <2624376436@qq.com>
2021-10-18 17:43:04 +0800

    Update index.tsx

--- src/pages/MergeCrossChain/index.tsx
+++ src/pages/MergeCrossChain/index.tsx
@@ -276,12 +276,15 @@ export default function CrossChain() {
 
   const outputBridgeValue = useMemo(() => {
     if (inputBridgeValue && destConfig) {
+      const baseFee = destConfig.BaseFeePercent ? (destConfig.MinimumSwapFee / (100 + destConfig.BaseFeePercent)) * 100 : 0
       const fee = Number(inputBridgeValue) * Number(destConfig.SwapFeeRatePerMillion) / 100
       let value = Number(inputBridgeValue) - fee
       if (fee < Number(destConfig.MinimumSwapFee)) {
         value = Number(inputBridgeValue) - Number(destConfig.MinimumSwapFee)
       } else if (fee > destConfig.MaximumSwapFee) {
         value = Number(inputBridgeValue) - Number(destConfig.MaximumSwapFee)
+      } else {
+        value = Number(inputBridgeValue) - fee - baseFee
       }
       if (value && Number(value) && Number(value) > 0) {
         return thousandBit(formatDecimal(value, Math.min(6, selectCurrency.decimals)), 'no')
@@ -290,7 +293,7 @@ export default function CrossChain() {
     } else {
       return ''
     }
-  }, [inputBridgeValue, destConfig, selectChain])
+  }, [inputBridgeValue, destConfig])
 
   const isWrapInputError = useMemo(() => {
     if (isRouter) {
