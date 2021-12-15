8d87ee53a38bb67fd3c5227521d93a4c77fba30b
d_winter <2624376436@qq.com>
2021-12-15 20:34:32 +0800

    Update eth.js

--- src/config/chainConfig/eth.js
+++ src/config/chainConfig/eth.js
@@ -148,7 +148,7 @@ const bridgeToken = {
     bridgeInitToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     bridgeInitChain: '56',
     nativeToken: '',
-    crossBridgeInitToken: 'ETH'
+    crossBridgeInitToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
   },
 }
 
