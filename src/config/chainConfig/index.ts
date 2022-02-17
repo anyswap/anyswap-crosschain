3f0ceea801a0885a9402255b023bf0597d028b7c
d_winter <2624376436@qq.com>
2022-02-17 13:39:20 +0800

    add new chain

--- src/config/chainConfig/index.ts
+++ src/config/chainConfig/index.ts
@@ -11,7 +11,7 @@ import kcc, {KCC_MAIN_CHAINID} from './kcc'
 import okt, {OKT_MAIN_CHAINID} from './okt'
 import one, {ONE_MAIN_CHAINID} from './one'
 import omgx, {OMGX_TEST_CHAINID} from './omgx'
-import optimism, {OPTIMISM_TEST_CHAINID} from './optimism'
+import optimism, {OPTIMISM_TEST_CHAINID, OPTIMISM_MAIN_CHAINID} from './optimism'
 import movr, {MOVR_MAIN_CHAINID} from './movr'
 import iotex, {IOTEX_MAIN_CHAINID} from './iotex'
 import sdn, {SDN_MAIN_CHAINID} from './sdn'
@@ -103,6 +103,7 @@ const allChainList = [
   FUSE_MAIN_CHAINID,
   ASTAR_MAIN_CHAINID,
   VLX_MAIN_CHAINID,
+  OPTIMISM_MAIN_CHAINID,
   ETH_TEST1_CHAINID,
 ]
 
