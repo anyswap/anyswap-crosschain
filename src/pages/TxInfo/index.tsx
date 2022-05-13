5fa08acafe7639041e9078c80262828c390ad4c6
shendel <im_shendel@inbox.ru>
2022-05-13 19:05:42 +0300

    fetch tx info and register if not founded

--- src/pages/TxInfo/index.tsx
+++ src/pages/TxInfo/index.tsx
@@ -1,17 +1,16 @@
-// @ts-ignore
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 // @ts-ignore
 import styled from 'styled-components'
-// @ts-ignore
 import { useTranslation } from 'react-i18next'
-// @ts-ignore
-import { useActiveWeb3React } from '../../hooks'
-// @ts-ignore
 import AppBody from '../AppBody'
 import { useAppState } from '../../state/application/hooks'
-// @ts-ignore
 import { getUrlData, postUrlData} from '../../utils/tools/axios'
+import { ButtonPrimary } from '../../components/Button'
+import { getEtherscanLink } from '../../utils'
+import { ExternalLink } from '../../theme/components'
+import config from '../../config'
+
 
 export default function TxInfo() {
   const {
@@ -22,36 +21,18 @@ export default function TxInfo() {
     apiAddress
   } = useAppState()
 
+  const { t } = useTranslation()
+
   const [isFetching, setIsFetching] = useState(false)
   const [isErrorFetch, setIsErrorFetch] = useState(false)
   const [txStatus, setTxStatus] = useState('')
   const [toChainId, setToChainId] = useState('')
   const [swapTx, setSwapTx] = useState('')
   const [txConfirmations, setTxConfirmations] = useState('')
+  const [notExists, setNotExists] = useState(false)
 
   if (false) console.log(isErrorFetch)
 
-  /*
-  export function registerSwap(hash: string, chainId: any, apiAddress: string) {
-    return new Promise((resolve, reject) => {
-      console.log(`Swap > chain id: ${chainId}; hash: ${hash}`)
-
-      const url = `${apiAddress}/swap/register/${chainId}/${hash}`
-
-      postUrlData(url, {
-        logindex: 0
-      })
-        .then((res: any) => {
-          console.log('Router response: ', res)
-          resolve(res)
-        })
-        .catch(error => {
-          console.error('Router error: ', error)
-          reject(error)
-        })
-    })
-  }
-  */
 
   const fetchTxStatus = () => {
     const url = `${apiAddress}/swap/status/${chainId}/${txId}`
@@ -62,18 +43,25 @@ export default function TxInfo() {
     getUrlData(url)
       .then((txStatus: any) => {
         console.log(txStatus)
+        setIsFetching(false)
         if (txStatus && txStatus?.msg && txStatus?.msg === `Success` && txStatus?.data) {
-          const {
-            swaptx,
-            toChainID,
-            status,
-            confirmations
-          } = txStatus.data
-          setTxStatus(status)
-          setToChainId(toChainID)
-          setSwapTx(swaptx)
-          setTxConfirmations(confirmations)
-          setIsFetching(false)
+          const data = txStatus.data
+          if (data === `mgoError: Swap is not found`) {
+            setNotExists(true)
+          }
+          if (data.swaptx !== undefined && data.status !== undefined) {
+            const {
+              swaptx,
+              toChainID,
+              status,
+              confirmations
+            } = txStatus.data
+            setTxStatus(status)
+            setToChainId(toChainID)
+            setSwapTx(swaptx)
+            setTxConfirmations(confirmations)
+            setNotExists(false)
+          }
         }
       })
       .catch(error => {
@@ -87,11 +75,49 @@ export default function TxInfo() {
     fetchTxStatus()
   }, [chainId, txId])
 
+  const [isDoRegisterSwap, setIsDoRegisterSwap] = useState(false)
+  const doRegisterSwap = () => {
+    console.log('>>> do register swap')
+    if (isDoRegisterSwap) return
+    setIsDoRegisterSwap(true)
+
+    const url = `${apiAddress}/swap/register/${chainId}/${txId}`
+
+    postUrlData(url, {
+      logindex: 0
+    })
+      .then((res: any) => {
+        console.log('Router response: ', res)
+        setIsDoRegisterSwap(false)
+        fetchTxStatus()
+      })
+      .catch(error => {
+        setIsDoRegisterSwap(false)
+        console.error('Router error: ', error)
+      })
+  }
+
   console.log('>>>>> urlParams', chainId, txId, apiAddress)
-  console.log('>>>>', txStatus, toChainId, swapTx, txConfirmations)
+  console.log('>>>>', notExists, txStatus, toChainId, swapTx, txConfirmations)
   return (
     <AppBody>
       <div>Tx info</div>
+      {((`${txStatus}` === `9` || `${txStatus}` === `10`) && swapTx) && (
+        <>
+          <h2>Ready</h2>
+          <ExternalLink href={getEtherscanLink(toChainId, swapTx, 'transaction')}>
+            {t('ViewOn')} {config.getCurChainInfo(toChainId).name}
+          </ExternalLink>
+        </>
+      )}
+      {notExists && (
+        <>
+          <h2>Transaction not found</h2>
+          <ButtonPrimary onClick={doRegisterSwap} disabled={isDoRegisterSwap}>
+            Add transaction for process
+          </ButtonPrimary>
+        </>
+      )}
     </AppBody>
   )
 }
