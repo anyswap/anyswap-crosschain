860384f58e2a199f0a6a669b31bff4dddbf2a25d
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-06-17 15:39:36 +0300

    update build

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c66abc628c253cc2a21b20025265ea0d.js"
+  "./precache-manifest.2bad6eecf98174d10a4284574e81fcfd.js"
 );
 
 self.addEventListener('message', (event) => {
