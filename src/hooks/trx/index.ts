f500f58cdb661592fbed240cf1131d2edc079587
d_winter <2624376436@qq.com>
2022-06-30 18:43:51 +0800

    Update index.ts

--- src/hooks/trx/index.ts
+++ src/hooks/trx/index.ts
@@ -64,7 +64,7 @@ export async function sendTRXTxns ({
         console.log(error)
         return {
           msg: 'Error',
-          error: error.toString()
+          error: error?.toString()
         }
       }
     } else {
